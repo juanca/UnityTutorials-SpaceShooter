4ccc27a0d006dd881dd688b504c7ebaf34b3483b
Juan Carlos Medina <juanca.med@gmail.com>
2016-11-29 13:28:37 -0800

    Create bolt prefab

---
+++ Assets/BoltController.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoltController : MonoBehaviour {
+
+  // Use this for initialization
+  void Start () {
+    Rigidbody rigidbody = GetComponent<Rigidbody> ();
+    rigidbody.velocity = transform.forward * 20;
+  }
+}
