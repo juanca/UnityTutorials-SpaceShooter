e1cfb97377412739b64a7c624cf243769ae87fa3
Juan Carlos Medina <juanca.med@gmail.com>
2016-12-02 14:04:04 -0800

    Implement destroyable asteroid

---
+++ Assets/AsteroidController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AsteroidController : MonoBehaviour {
+  void Start () {
+    Rigidbody rigidbody = GetComponent<Rigidbody> ();
+    rigidbody.angularVelocity = 5 * Random.insideUnitSphere;
+  }
+
+  void OnTriggerEnter (Collider other) {
+    if (other.CompareTag ("Boundary")) {
+      return;
+    }
+
+    Destroy (other.gameObject);
+    Destroy (gameObject);
+  }
+}
