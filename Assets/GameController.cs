b7b6c9022b1ec858f7ff50d5cf9dda7bcdce8ef5
Juan Carlos Medina <juanca.med@gmail.com>
2016-12-04 15:12:31 -0800

    Implement waves of hazards via coroutines

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,8 +5,22 @@ public class GameController : MonoBehaviour {
   public GameObject hazard;
 
   void Start () {
-    Vector3 hazardPosition = new Vector3 (Random.Range (-6.0f, 6.0f), 0.0f, 18.5f);
-    Quaternion hazardRotation = Quaternion.identity;
-    Instantiate (hazard, hazardPosition, hazardRotation);
+    StartCoroutine (SpawnWaves ());
+  }
+
+  IEnumerator SpawnWaves () {
+    yield return new WaitForSeconds (3.0f);
+
+    while (true) {
+      for (int i = 0; i < 10; i += 1) {
+        Vector3 hazardPosition = new Vector3 (Random.Range (-6.0f, 6.0f), 0.0f, 18.5f);
+        Quaternion hazardRotation = Quaternion.identity;
+        Instantiate (hazard, hazardPosition, hazardRotation);
+
+        yield return new WaitForSeconds (0.5f);
+      }
+
+      yield return new WaitForSeconds (2.0f);
+    }
   }
 }
