d8172af2d84410e3b2bf875b0dc88f4c4afa9f39
Juan Carlos Medina <juanca.med@gmail.com>
2016-12-04 14:27:59 -0800

    Move asteroid down the screen (toward player)

--- Assets/AsteroidController.cs
+++ Assets/AsteroidController.cs
@@ -8,6 +8,7 @@ public class AsteroidController : MonoBehaviour {
   void Start () {
     Rigidbody rigidbody = GetComponent<Rigidbody> ();
     rigidbody.angularVelocity = 5 * Random.insideUnitSphere;
+    rigidbody.velocity = -5 * transform.forward;
   }
 
   void OnTriggerEnter (Collider other) {
