ef1473c6c6002419fa2216c97a87f698cdbcb235
Juan Carlos Medina <juanca.med@gmail.com>
2016-12-04 14:50:28 -0800

    Implement GameController to spawn a hazard

---
+++ Assets/GameController.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+  public GameObject hazard;
+
+  void Start () {
+    Vector3 hazardPosition = new Vector3 (Random.Range (-6.0f, 6.0f), 0.0f, 18.5f);
+    Quaternion hazardRotation = Quaternion.identity;
+    Instantiate (hazard, hazardPosition, hazardRotation);
+  }
+}
