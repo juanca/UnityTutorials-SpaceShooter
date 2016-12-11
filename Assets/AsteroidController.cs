27b640856ce4944ebfc97036159dacc579b0ddd8
Juan Carlos Medina <juanca.med@gmail.com>
2016-12-11 15:16:40 -0800

    Extract DestroyByContact script from AsteroidController
    To be reused with enemy ship

--- Assets/AsteroidController.cs
+++ Assets/AsteroidController.cs
@@ -1,34 +1,9 @@
 ﻿using UnityEngine;
 
 public class AsteroidController : MonoBehaviour {
-  public GameObject asteroidExplosion;
-  public GameObject playerExplosion;
-
-  private GameController gameController;
-
   void Start () {
-    GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
-    gameController = gameControllerObject.GetComponent<GameController> ();
-
     Rigidbody rigidbody = GetComponent<Rigidbody> ();
     rigidbody.angularVelocity = 5 * Random.insideUnitSphere;
     rigidbody.velocity = -5 * transform.forward;
-  }
-
-  void OnTriggerEnter (Collider other) {
-    if (other.CompareTag ("Boundary")) {
-      return;
-    }
-
-    if (other.CompareTag ("Player")) {
-      Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
-      gameController.GameOver ();
-    }
-
-    Instantiate (asteroidExplosion, transform.position, transform.rotation);
-    Destroy (other.gameObject);
-    Destroy (gameObject);
-
-    gameController.IncrementScore (10);
   }
 }
---
+++ Assets/Scripts/DestroyByContact.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByContact : MonoBehaviour {
+  private GameController gameController;
+
+  public GameObject selfExplosion;
+  public GameObject playerExplosion;
+
+
+  void Start () {
+    GameObject gameControllerObject = GameObject.FindWithTag ("GameController");
+    gameController = gameControllerObject.GetComponent<GameController> ();
+  }
+
+  void OnTriggerEnter (Collider other) {
+    if (other.CompareTag ("Boundary")) {
+      return;
+    }
+
+    if (other.CompareTag ("Player")) {
+      Instantiate (playerExplosion, other.transform.position, other.transform.rotation);
+      gameController.GameOver ();
+    }
+
+    Instantiate (selfExplosion, transform.position, transform.rotation);
+    Destroy (other.gameObject);
+    Destroy (gameObject);
+
+    gameController.IncrementScore (10);
+  }
+}
