69b0cfb9aa8063de02bb5ae2ad23637ec35a670c
Juan Carlos Medina <juanca.med@gmail.com>
2016-12-11 14:33:47 -0800

    Implement scrolling background

---
+++ Assets/BackgroundController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class BackgroundController : MonoBehaviour {
+  private Vector3 startPosition;
+
+  void Start () {
+    startPosition = transform.position;
+  }
+
+  void Update () {
+    float newPosition = Mathf.Repeat (Time.time * -0.25f, 18);
+    transform.position = startPosition + newPosition * Vector3.forward;
+  }
+}
